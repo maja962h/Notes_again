4ab7dc6188e1a1ba6a08b565a9b75ec8acef2859
majamatboue <maja962h@stud.kea.dk>
2023-02-21 10:37:16 +0100

    Initial Commit

---
+++ Notes_again/Notes_againApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_againApp.swift
+//  Notes_again
+//
+//  Created by Maja Matboue on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Notes_againApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
