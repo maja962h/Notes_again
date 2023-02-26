d48450811059f6f8ceb7a782684444e8e1a8ca6a
majamatboue <maja962h@stud.kea.dk>
2023-02-26 11:44:56 +0100

    first commit

---
+++ Notes_again/FirebaseService.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseService.swift
+//  Notes_again
+//
+//  Created by Maja Matboue on 21/02/2023.
+//
+
+import Foundation
---
+++ Notes_again/Preview Content/Note.swift
@@ -0,0 +1,20 @@
+//
+//  Note.swift
+//  Notes_again
+//
+//  Created by Maja Matboue on 21/02/2023.
+//
+
+import SwiftUI
+
+struct Note: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Note_Previews: PreviewProvider {
+    static var previews: some View {
+        Note()
+    }
+}
